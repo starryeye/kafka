87d4028ed198edbf0810cc30e599f51c5c695558
starryeye <see2818@naver.com>
2023-06-25 03:37:46 +0900

    [starryeye, 2023.06.25] - Spring Kafka client study init
    내용
    - Spring Kafka client study init

---
+++ HelloKafkaWithSpring/src/main/java/dev/practice/HelloKafkaWithSpring/HelloKafkaWithSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.HelloKafkaWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloKafkaWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloKafkaWithSpringApplication.class, args);
+	}
+
+}
---
+++ HelloKafkaWithSpring/src/test/java/dev/practice/HelloKafkaWithSpring/HelloKafkaWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.HelloKafkaWithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKafkaWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
