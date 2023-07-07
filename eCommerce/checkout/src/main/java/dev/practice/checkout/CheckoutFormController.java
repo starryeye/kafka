b06e02ebe41154f3171ba45998e4823c47086270
starryeye <see2818@naver.com>
2023-07-08 03:39:42 +0900

    [starryeye, 2023.07.08] - checkoutForm Controller, Form 개발
    내용
    - 고객이 상품 구매를 위해 작성할 form 진입 요청처리 controller
    - 요청의 응답으로 상품 구매를 위한 form 이 응답으로 나간다.
    - form 에는 상품 구매 정보들을 입력할수 있는 칸이 존재하고 제출 버튼(post) 가 있다.

---
+++ eCommerce/checkout/src/main/java/dev/practice/checkout/CheckoutFormController.java
@@ -0,0 +1,17 @@
+package dev.practice.checkout;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class CheckoutFormController {
+
+    @GetMapping("checkout/form")
+    public String checkoutForm(Model model) {
+        log.info("checkoutForm");
+        return "checkoutForm";
+    }
+}
