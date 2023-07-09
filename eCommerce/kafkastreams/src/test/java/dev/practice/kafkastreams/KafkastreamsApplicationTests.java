9bb76d714026a9932f1f6930497b6787b75ceeb6
starryeye <see2818@naver.com>
2023-07-09 20:58:02 +0900

    [starryeye, 2023.07.09] - Kafka stream project init
    내용
    - kafka stream 을 통해 kafka 로 인입된 메시지를 실시간으로 집계하는 역할한다.
    - 메시지를 소비하고 발행할 수 있다.

---
+++ eCommerce/kafkastreams/src/test/java/dev/practice/kafkastreams/KafkastreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.kafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkastreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
